6aa26a880665c5e2aa8f09641f51484376a975d0
thejackbrand <thejackbrand.psu@gmail.com>
2024-10-21 16:40:34 -0400

    xcode init
    init xcode into repo

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jack Brand on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
