5ca85d3b3478f73c023ce1f16b0a9e298e1c408a
thejackbrand <thejackbrand.psu@gmail.com>
2024-10-29 12:16:47 -0400

    permission priming ui

---
+++ Step Tracker/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,45 @@
+//
+//  HealthKitPermissionPrimingView.swift
+//  Step Tracker
+//
+//  Created by Jack Brand on 10/29/24.
+//
+
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    var description = """
+    This app displays your step and weight data in interactive charts.
+    
+    You can also add new step or weight data to Apple Health from this app. Your data is private and secured.
+    """
+    
+    var body: some View {
+        VStack(spacing: 130) {
+            VStack(alignment: .leading, spacing: 10) {
+                Image(.appleHealthIcon)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3), radius: 16)
+                    .padding(.bottom, 12)
+                
+                Text("Apple Health Integration")
+                    .font(.title2.bold())
+                
+                Text(description)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Button("Connect Apple Health") {
+                // do later
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
